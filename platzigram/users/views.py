000a3a81383e57e4d886d676de81dc13c539c079
hittwar <hittwar@hotmail.com>
2020-07-13 12:00:38 -0500

    updated profile and middleware

--- platzigram/users/views.py
+++ platzigram/users/views.py
@@ -5,8 +5,46 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
+# Exception
+from django.db.utils import IntegrityError
+
+# Models
+from django.contrib.auth.models import User
+from users.models import Profile
+
+# Forms
+from users.forms import ProfileForm
 
 # Create your views here.
+
+def update_profile(request):
+    """update a user's profile view"""
+    profile = request.user.profile
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            data = form.cleaned_data
+            
+            profile.website = data['website']
+            profile.phone_number = data['phone_number']
+            profile.biography = data['biography']
+            profile.picture = data['picture']
+            profile.save()
+
+            return redirect('update_profile')
+    else:
+        form = ProfileForm()
+
+    return render(
+        request = request,
+        template_name='users/update_profile.html',
+        context={
+            'profile':profile,
+            'user': request.user,
+            'form': form
+        }
+    )
+
 
 def login_view(request):
     """Login view."""
@@ -27,3 +65,33 @@ def logout_view(request):
     """Logout User"""
     logout(request)
     return redirect('login')
+
+def signup(request):
+    """Signup view"""
+    if request.method == 'POST':
+        username = request.POST['username']
+        passwd = request.POST['passwd']
+        passwd_confirmation = request.POST['passwd_confirmation']
+
+        if passwd != passwd_confirmation:
+            return render(request,'users/signup.html',{'error':'Password confirmation does not match'})
+        
+        try:
+            user = User.objects.create_user(username=username, password=passwd)
+        except IntegrityError:
+            return render(request, 'users/signup.html', {'error': 'Username is already in user'})
+
+        user.first_name = request.POST['first_name']
+        user.last_name = request.POST['last_name']
+        user.email = request.POST['email']
+        user.save()
+
+        profile = Profile(user=user)
+        profile.save()
+
+        return redirect('login')
+
+    return render(request, 'users/signup.html')
+
+
+
